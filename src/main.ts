89aa86020b727f5e773c2bd2c76ae06032ff2652
DuyTran1811 <ferssken99@gmail.com>
2021-04-28 12:01:28 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+function isPrime(number: number): boolean {
+    let isPrime = true;
+    if (number < 2) {
+        isPrime = false;
+    } else if (number > 2) {
+        for (let i = 2; i < Math.sqrt(i); i++) {
+            if (i % number == 0) {
+                return isPrime = false;
+                break;
+            }
+        }
+    }
+    return isPrime;
+}
+let array = [1, 5, 9, 2, 6, 15, 19, 35, 51, 53];
+let sum = 0;
+for (let number of array) {
+    if (isPrime(number)) {
+        sum += number;
+    }
+}
+console.log("Tổng các số nguyên tố trong mảng trên là: " + sum);
